41ddab57255a306dbf8627ef2175a45070377c5e
Gorills <gorivanickiy@gmail.com>
2020-09-07 18:49:00 +0700

    pages optimize

--- resources/views/teploizolyaciya/bolshih-vertikalnyh-emkostej.blade.php
+++ resources/views/teploizolyaciya/bolshih-vertikalnyh-emkostej.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.custom')
 
-@section('title', 'Теплоизоляция трубопроводов пара и отопления | ООО Изотермакс')
-@section('description', 'Расчитать стоимость теплоизоляции трубопроводов пара и отопления с выездом ')
+@section('title', 'Теплоизоляция вертикальных емкостей | ООО Изотермакс')
+@section('description', 'Расчитать стоимость теплоизоляции вертикальных емкостей с выездом. Звоните: 8 (3822) 941-199')
 @section('keywords', '')
-@section('canonical', '')
-@section('og:title', '')
-@section('og:description', '')
-@section('og:url', '')
-@section('twitter:title', '')
-@section('twitter:description', '')
+@section('canonical', 'https://izotermax.ru/promyshlennaya-teploizolyaciya/bolshih-vertikalnyh-emkostej')
+@section('og:title', 'Теплоизоляция вертикальных емкостей | ООО Изотермакс')
+@section('og:description', 'Расчитать стоимость теплоизоляции вертикальных емкостей с выездом. Звоните: 8 (3822) 941-199')
+@section('og:url', 'https://izotermax.ru/promyshlennaya-teploizolyaciya/bolshih-vertikalnyh-emkostej')
+@section('twitter:title', 'Теплоизоляция вертикальных емкостей | ООО Изотермакс')
+@section('twitter:description', 'Расчитать стоимость теплоизоляции вертикальных емкостей с выездом. Звоните: 8 (3822) 941-199')
 
 
 @section('content')
 
-    <?php $pages = 'Трубопроводов пара, отопления' ?>
+    <?php $pages = 'Теплоизоляция вертикальных емкостей' ?>
 
     @component('components.breadcrumb')
         {{--        @slot('title') Промышленная теплоизоляция @endslot--}}
@@ -23,8 +23,8 @@
                 @slot('prev') Промышленная теплоизоляция @endslot
         @slot('link') {{ url('/promyshlennaya-teploizolyaciya') }} @endslot
 
-        @slot('active') Трубопроводов пара, отопления @endslot
-        @slot('activelink') {{ url('/promyshlennaya-teploizolyaciya/truboprovodov-para-otopleniya') }} @endslot
+        @slot('active') Теплоизоляция вертикальных емкостей @endslot
+        @slot('activelink') {{ url('/promyshlennaya-teploizolyaciya/bolshih-vertikalnyh-emkostej') }} @endslot
 
 
 
@@ -34,38 +34,22 @@
         <div class="container">
             <div class="page__inner">
 
-                <h1 class="title">Теплоизоляция трубопроводов пара и отопления</h1>
+                <h1 class="title">Теплоизоляция вертикальных емкостей</h1>
 
-                <img src="{{ URL::asset('/images/uslugi/1.png') }}" alt="Теплоизоляция трубопроводов пара и отопления" class="page__img">
+                <img src="{{ URL::asset('/images/6/1.png') }}" alt="Теплоизоляция вертикальных емкостей" class="page__img">
 
 
                 <p class="page__text">
-                    <strong>Цены от 670 руб. за м.пог.</strong>
-                </p>
-                <p class="page__text">
-                    Ддя эффективной работы предприятия и для уменьшения потерь тепла производится теплоизоляция теплоизоляция трубопроводов пара,
-                    отопления, горячей воды, газоходов. Это позволяет значительно сократить издержки на потерю тепла.
-                </p>
-                <p class="page__text">
-                    На заводах и предприятиях, в котельных  теплоизоляция трубопроводов пара  производится  минераловатными матами ,
-                    цилиндрами  разных производителей толщиной от 50 до 150мм  в зависимости от  диаметра трубопроводов. При изоляции
-                    трубопроводов отопления используются  минераловатные маты, цилиндры  толщиной 50мм-80мм, также  используется при изоляции
-                    отопления K-flex, Энергофлекс.
+                    <strong>Цены от 3450 руб. за м.кв.</strong>
                 </p>
 
 
-                <h2 class="page__title">Расчитать стоимость теплоизотяции трубопроводов отопления и пара.</h2>
 
-                <p class="page__text">
-                    Для расчета стоимости теплоизотяции трубопроводов отопления и пара на Вашем объекте просто заполните форму ниже и наши специалисты свяжутся с Вами в
-                    кратчайшие сроки. Не имеет значения в каком регионе Вы находитесь - мы сможем удивить Вас приятными ценами и качеством наших
-                    услуг.
-                </p>
+                @component('components.text')
+                    @slot('title')теплоизоляции вертикальных емкостей @endslot
+                    @slot('subtitle') теплоизоляции вертикальных емкостей @endslot
 
-                <p class="page__text">
-                    Так же мы принимаем заявки на расчет стоимости на нашу почту <a href="mailto:ferrum_art@mail.ru">ferrum_art@mail.ru</a> и по телефону
-                    <a href="tel:83822941199">8 (3822) 941-199</a>.
-                </p>
+                @endcomponent
 
             </div>
 
